ed838c68826206c6fb0985060179f1f3b9e90bbf
ruokunan <ruokua@ougl-09-a.eplt.washington.edu>
2015-04-21 14:32:19 -0700

    add location to UW

--- app/src/main/java/edu/washington/ruokua/geoappmodule/MainActivity.java
+++ app/src/main/java/edu/washington/ruokua/geoappmodule/MainActivity.java
@@ -1,39 +1,57 @@
 package edu.washington.ruokua.geoappmodule;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+    private final LatLng LOCATION_UW = new LatLng(47.6550, -122.3080);
+    //private final LatLng LOCATION_SURRREY = new LatLng(49.187500, -122.849000);
+
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        map  = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+
+        //map.addMarker(new MarkerOptions().position(LOCATION_SURRREY).title("Find me here!"));
+    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+
+    public void onClick_City(View v) {
+//		CameraUpdate update = CameraUpdateFactory.newLatLng(LOCATION_BURNABY);
+        map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(LOCATION_UW, 9);
+        map.animateCamera(update);
+    }
+    public void onClick_Burnaby(View v) {
+        map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(LOCATION_UW, 14);
+        map.animateCamera(update);
+
+    }
+    public void onClick_Surrey(View v) {
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+       // CameraUpdate update = CameraUpdateFactory.newLatLngZoom(LOCATION_SURRREY, 16);
+       // map.animateCamera(update);
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
